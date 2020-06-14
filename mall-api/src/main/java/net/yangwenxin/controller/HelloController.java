e75750813425464c4d2612e158fbbe09df46eed7
yangwenxin45 <yangwenxin45@gmail.com>
2020-06-14 10:50:04 +0800

    整合log4j打印日志 通过日志监控service运行时间 开启MyBatis日志Sql打印

--- mall-api/src/main/java/net/yangwenxin/controller/HelloController.java
+++ mall-api/src/main/java/net/yangwenxin/controller/HelloController.java
@@ -1,5 +1,6 @@
 package net.yangwenxin.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -9,10 +10,15 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 @ApiIgnore
+@Slf4j
 public class HelloController {
 
     @GetMapping("/hello")
     public Object Hello() {
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
         return "Hello World~";
     }
 
